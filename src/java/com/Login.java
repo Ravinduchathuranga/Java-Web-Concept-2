0b5cbe22b483abc7f2101ba70a1e33eea19d67cc
Ravinduchathuranga <ravicdasanayaka@gmail.com>
2024-03-20 11:10:10 +0530

    service method demostration

--- src/java/com/Login.java
+++ src/java/com/Login.java
@@ -1,9 +1,26 @@
 package com;
 
+import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @WebServlet(name = "Login", value = "/Login")
 public class Login extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String method = req.getMethod().toUpperCase();
+        switch (method) {
+            case "GET":
+                doGet(req, resp);
+                break;
+            case "POST":
+                doPost(req, resp);
+                break;
+        }
+    }
 
 }
