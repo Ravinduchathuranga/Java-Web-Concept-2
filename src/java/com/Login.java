9e8fa68c768df20b9bc259bee29abdcde38442c4
Ravinduchathuranga <ravicdasanayaka@gmail.com>
2024-03-20 10:12:29 +0530

    Login page added!

---
+++ src/java/com/Login.java
@@ -0,0 +1,9 @@
+package com;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+
+@WebServlet(name = "Login", value = "/Login")
+public class Login extends HttpServlet {
+
+}
